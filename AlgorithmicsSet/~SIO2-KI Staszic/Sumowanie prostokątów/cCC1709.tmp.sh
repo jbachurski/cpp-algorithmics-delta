da84204a329c2d925cd68dc744409020fcfd7c78
jbachurski <kbachurski@gmail.com>
2019-10-23 19:57:40 +0200

    Sumowanie prostokątów. Przeklęta praca naukowa!

--- AlgorithmicsSet/~SIO2-KI Staszic/Sumowanie prostokątów/cCC1709.tmp.sh
+++
@@ -1,6 +0,0 @@
-trap exit INT
-./83AaeEA.tmp.exe | tee eB1ac1e.tmp.out
-sleep 0.033s
-printf "\n\n[Exited with code $?]\n"
-read -rsn1 -p "Press any key to continue . . ."
-echo
--- AlgorithmicsSet/~SIO2-KI Staszic/Sumowanie prostokątów/sup.cpp
+++ AlgorithmicsSet/~SIO2-KI Staszic/Sumowanie prostokątów/sup.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const size_t MAX = 1 << 30;
+const size_t MAX = 1 << 18;
 
 struct fenwick_tree
 {
@@ -26,19 +26,19 @@ struct fenwick_tree
 
 struct fenwick_tree_2d
 {
-    unordered_map<size_t, fenwick_tree> F;
+    vector<fenwick_tree> F;
     static constexpr size_t lsb(size_t x) { return x & -x; }
 
+    fenwick_tree_2d() : F(MAX) {}
     uint get_prefix(size_t x, size_t y)
     {
         uint r = 0;
         while(y)
-            r += F.count(y) ? F[y].get_prefix(x) : 0, y -= lsb(y);
+            r += F[y].get_prefix(x), y -= lsb(y);
         return r;
     }
     void delta(size_t x, size_t y, uint v)
     {
-        cout << "(" << x << ", " << y << ") += " << v << endl;
         while(y < MAX)
             F[y].delta(x, v), y += lsb(y);
     }
@@ -58,7 +58,7 @@ int main()
     size_t q;
     cin >> q;
 
-    const size_t n = MAX-10;
+    const size_t n = MAX;
     fenwick_tree_2d bitxy, bitx, bity, bit1;
     auto update = [&](uint x1, uint y1, uint x2, uint y2, uint c) {
        bitxy.deltar(x1,   y1,   x2, y2, c);
@@ -66,19 +66,17 @@ int main()
         bitx.deltar(x1,   y2+1, x2, n,  c*(y2 - y1 + 1));
         bity.deltar(x1,   y1,   x2, y2, -c*(x1 - 1));
         bity.deltar(x2+1, y1,   n,  y2, c*(x2 - x1 + 1));
-        bit1.deltar(x1,   y1,   x2, y2, c);
+        bit1.deltar(x1,   y1,   x2, y2, c*(x1 - 1)*(y1 - 1));
         bit1.deltar(x2+1, y1,   n,  y2, -c*(y1 - 1)*(x2 - x1 + 1));
         bit1.deltar(x1,   y2+1, x2, n,  -c*(x1 - 1)*(y2 - y1 + 1));
         bit1.deltar(x2+1, y2+1, n,  n,  c*(x2 - x1 + 1)*(y2 - y1 + 1));
     };
     auto queryp = [&](uint x, uint y) {
         auto a = bitxy.get_prefix(x, y), b = bitx.get_prefix(x, y), c = bity.get_prefix(x, y), d = bit1.get_prefix(x, y);
-        cout << a << "/" << b << "/" << c << "/" << d << endl;
         return a*x*y + b*x + c*y + d;
     };
     auto query = [&](size_t x1, size_t y1, size_t x2, size_t y2) {
-        return queryp(x1-1, y1-1) + queryp(x2, y2) -
-               queryp(x2, y1-1) - queryp(x1-1, y2);
+        return queryp(x1-1, y1-1) + queryp(x2, y2) - queryp(x2, y1-1) - queryp(x1-1, y2);
     };
 
     while(q --> 0)
@@ -94,6 +92,5 @@ int main()
         }
         else if(t == 2)
             cout << query(x1, y1, x2, y2) << '\n';
-        cout << queryp(1, 1) << '\n';
     }
 }
