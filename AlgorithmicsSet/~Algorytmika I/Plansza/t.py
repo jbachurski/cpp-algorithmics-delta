f3a6d589be2b3595d99c1413591cf13030ccfaef
jbachurski <kbachurski@gmail.com>
2019-12-23 00:15:45 +0100

    Nic tu nie ma

--- AlgorithmicsSet/~Algorytmika I/Plansza/t.py
+++
@@ -1,9 +0,0 @@
-import os
-
-while True:
-    os.system("python gen.py > t.in")
-    code = os.system("./b < t.in")
-    print("!", code)
-    if code:
-        print(open("t.in").read().strip())
-        break
