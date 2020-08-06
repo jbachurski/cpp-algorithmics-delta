27798fabdd37a0bcd5f166ab49d286fa2bdfb0c8
jbachurski <kbachurski@gmail.com>
2020-08-06 21:21:27 +0200

    Tego tu nie trzeba

--- AlgorithmicsSet/~ONTAK/Wipeout/wip.cpp
+++ AlgorithmicsSet/~ONTAK/Wipeout/wip.cpp
@@ -2,40 +2,6 @@
 
 using namespace std;
 
-template<typename T, typename BinaryOperation>
-struct sparse_table
-{
-    struct _identity { T operator() (const T& x) const { return x; } };
-    BinaryOperation F;
-    size_t n;
-    vector<vector<T>> A;
-    template<typename Iterator>
-    sparse_table(Iterator first, Iterator last, BinaryOperation f = {}) : F(f)
-    {
-        n = distance(first, last);
-        A.emplace_back(distance(first, last));
-        for(size_t i = 0; first != last; first++, i++)
-            A[0][i] = *first;
-        for(size_t i = 1; (1u << i) <= n; i++)
-        {
-            A.emplace_back(n - (1u << i) + 1);
-            for(size_t j = 0; j + (1 << i) <= n; j++)
-                A[i][j] = F(A[i-1][j], A[i-1][j + (1u << (i-1))]);
-        }
-    }
-    T operator() (size_t a, size_t b) const
-    {
-        size_t p = __lg(b - a);
-        return F(A[p][a], A[p][b - (1 << p)]);
-    }
-};
-template<typename T>
-struct min_f { T operator() (const T& a, const T& b) const { return min(a, b); } };
-using min_table = sparse_table<int64_t, min_f<int64_t>>;
-template<typename T>
-struct max_f { T operator() (const T& a, const T& b) const { return max(a, b); } };
-using max_table = sparse_table<int64_t, max_f<int64_t>>;
-
 vector<size_t> parent, preorder, postorder;
 vector<vector<size_t>> children;
 vector<int> value;
